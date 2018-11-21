0e6382a58202ee7186266ece3aac271176c99c39
Gabriel <pgabr001@fiu.edu>
2018-11-21 12:22:47 -0500

    Added main.cpp file.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_1337
+//
+//  Created by Perry Gabriel on 11/19/18.
+//  Copyright © 2018 Group_09. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
